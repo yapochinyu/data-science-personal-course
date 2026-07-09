8b5bc6d286848521137391dd370d3d66cbf1a436
Valeriy Kabisov <yapochinyu.nasnet@gmail.com>
2026-07-10 00:12:01 +0300

    reviewed some solutions

--- 5. args kwargs/12.py
+++ 5. args kwargs/12.py
@@ -1,9 +1,14 @@
 base = {'lr': 0.01, 'epochs': 100, 'batch_size': 32}
 experiments = [{'lr': 0.1}, {'batch_size': 64}, {'lr': 0.001, 'epochs': 200}]
 
+def model_fn(**kwargs):
+    return kwargs
+
 
 def run_all(model_fn, base, experiments):
     results = []
     for experiment in experiments:
-        results.append(model_fn(**{**base, **experiment}))
+        results.append(model_fn(**(base | experiment)))
     return results
+
+print(run_all(model_fn, base, experiments))
